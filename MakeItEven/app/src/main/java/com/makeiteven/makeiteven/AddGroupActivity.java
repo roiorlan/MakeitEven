407becbfacfdef9e98aebf8efbff373357fa7d21
dkorn <dkorn@redhat.com>
2014-11-23 14:33:15 +0200

    Created Add Group button that respond to a click
    Starts a new activity on click: activity_add_group.
    Current color is purple. for green: ff50b93b

--- MakeItEven/app/src/main/java/com/makeiteven/makeiteven/AddGroupActivity.java
+++ MakeItEven/app/src/main/java/com/makeiteven/makeiteven/AddGroupActivity.java
@@ -3,33 +3,34 @@ package com.makeiteven.makeiteven;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Build;
 
 
-public class MainActivity extends Activity {
+public class AddGroupActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_group);
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_add_group, menu);
         return true;
     }
 
@@ -48,12 +49,6 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user press the add group button */
-    public void addGroup(View view) {
-        Intent intent = new Intent(this, AddGroupActivity.class);
-        startActivity(intent);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -65,9 +60,8 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_add_group, container, false);
             return rootView;
         }
     }
-
 }
